306cb5c6b91acadf37e995ef3041dddf1de63aca
Level Drain <drain2291@gmail.com>
2020-06-14 02:11:53 +0900

    チェリーちゃんのソースコード

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <h1>{{ ttl }}</h1>
+    <img alt="Vue logo" src="./assets/cherry-chan.png" />
+    <Talk msg2="古代兵装・兜" />
+  </div>
+</template>
+
+<script>
+import Talk from "./components/Talk.vue";
+
+export default {
+  data() {
+    return {
+      ttl: "ワシのかわいい　チェリーちゃん"
+    };
+  },
+  components: {
+    Talk
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Talk.vue
@@ -0,0 +1,18 @@
+<template>
+  <p>{{ msg[0] + msg2 + msg[1] }}</p>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: ["マイド ", " ニ変換スルンヤナ ワレ"] //App.vue へ出力
+    };
+  },
+  props: {
+    msg2: String //App.vue からの入力
+  }
+};
+</script>
+
+<style></style>
